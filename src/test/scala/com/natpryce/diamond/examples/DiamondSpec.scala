bb6d695db61e0db76ada3d7f4eea809fc658fab5
Nat Pryce <sw@natpryce.com>
2014-12-29 22:06:22 +0000

    Specialise the property -- the number of lines must be odd.
    This implies greater than zero, so I changed the test rather than adding a new one.
    
    But the test still passes: my current implementation returns a single line, and 1 is an odd number.  Not a great choice of property.
    
    I checked that the test failed if the property was that the number of lines should be even, so I'm happy the test works. But it does not help move the implementation forward.
    
    I'll keep the new test anyway, since it works and does constrain the implementation more tightly.

--- src/test/scala/com/natpryce/diamond/examples/DiamondSpec.scala
+++ src/test/scala/com/natpryce/diamond/examples/DiamondSpec.scala
@@ -5,11 +5,13 @@ import org.scalacheck._
 class DiamondSpec extends UnitSpec {
   val inputChar = Gen.alphaUpperChar
 
-  "produces some lines" in {
-    forAll (inputChar) { c => assert(diamondLines(c).nonEmpty) }
+  "produces an odd number lines" in {
+    forAll (inputChar) { c => assert(isOdd(diamondLines(c).length)) }
   }
 
   def diamondLines(c : Char) = {
     Diamond.diamond(c).lines.toVector
   }
+
+  def isOdd(n : Int) = n % 2 == 1
 }
