39a87a670456e59151cc9d06a03627890b750bda
Nat Pryce <sw@natpryce.com>
2015-01-04 14:08:50 +0000

    Make the test pass
    Time for some refactoring, of both the tests and the implementation...

--- src/main/scala/com/natpryce/diamond/examples/Diamond.scala
+++ src/main/scala/com/natpryce/diamond/examples/Diamond.scala
@@ -4,14 +4,19 @@ object Diamond {
   def diamond(c: Char) : String = {
     val side: Int = squareSide(c)
 
-    val topHalf = for (lc <- 'A' to c) yield lineFor(side, lc)
-    val bottomHalf = topHalf.slice(0, topHalf.length-1).reverse
-    (topHalf ++ bottomHalf).mkString("\n")
+    val topHalf = for (letter <- 'A' to c) yield lineFor(side, letter)
+    (topHalf ++ topHalf.reverse.tail).mkString("\n")
   }
 
-  def lineFor(side: Int, lc: Char): String = {
-    lc.toString * side
+  def lineFor(length: Int, letter: Char): String = {
+    val halfLength = length/2
+    val letterIndex = halfLength - ord(letter)
+    val halfLine = " "*letterIndex + letter + " "*(halfLength-letterIndex)
+
+    halfLine ++ halfLine.reverse.tail
   }
 
-  def squareSide(c: Char) : Int = 2*(c - 'A') + 1
+  def squareSide(c: Char) : Int = 2*ord(c) + 1
+
+  def ord(c: Char): Int = c - 'A'
 }
