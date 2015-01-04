c0cf5793c2173055567d2571806b8f67594e062d
Nat Pryce <sw@natpryce.com>
2015-01-04 14:08:50 +0000

    Another failing test  - specify the position and value of the letter in each line, and that all other characters in a line are spaces  - rely on horizontal symmetry, so add a spec that the output should be horizontally symmetrical  - refactor commonality between tests for horizontal and vertical symmetry into generic functions
    implicits magic cribbed from here http://stackoverflow.com/questions/10689161/method-taking-seqt-to-return-string-rather-than-seqchar

--- src/test/scala/com/natpryce/diamond/examples/DiamondSpec.scala
+++ src/test/scala/com/natpryce/diamond/examples/DiamondSpec.scala
@@ -3,6 +3,8 @@ package com.natpryce.diamond.examples
 import com.natpryce.diamond.examples.Diamond.squareSide
 import org.scalacheck._
 
+import scala.collection.generic.CanBuildFrom
+
 class DiamondSpec extends UnitSpec {
   val inputChar = Gen.alphaUpperChar
 
@@ -28,15 +30,43 @@ class DiamondSpec extends UnitSpec {
       }
     }
   }
-  
-  "is vertically symmetrical" in {
-    forAll(inputChar) { c =>
-      val allLines = diamondLines(c)
-      val topHalf = allLines.slice(0, allLines.size / 2 + 1)
-      val bottomHalf = allLines.slice(allLines.size / 2, allLines.size)
 
-      assert(topHalf == bottomHalf.reverse)
+  "is vertically symmetrical" in {
+    forAll (inputChar) { c =>
+      val lines = diamondLines(c)
+
+      assert(firstHalfOf(lines) == secondHalfOf(lines).reverse)
     }
+  }
+  
+  "is horizontally symmetrical" in {
+    forAll (inputChar) { c =>
+      for ((line, index) <- diamondLines(c).zipWithIndex) {
+        assert(firstHalfOf(line) == secondHalfOf(line).reverse,
+          "line " + index + " should be symmetrical")
+      }
+    }
+  }
+
+  "position of letter in line of spaces" in {
+    forAll (inputChar) { c =>
+      for ((line, lineIndex) <- firstHalfOf(diamondLines(c)).zipWithIndex) {
+        val firstHalf = firstHalfOf(line)
+        val expectedLetter = ('A'+lineIndex).toChar
+        val letterIndex = firstHalf.length - (lineIndex + 1)
+
+        assert (firstHalf(letterIndex) == expectedLetter, firstHalf)
+        assert (firstHalf.count(_==' ') == firstHalf.length-1, "number of spaces in line " + lineIndex + ": " + line)
+      }
+    }
+  }
+
+  def firstHalfOf[AS, A, That](v: AS)(implicit asSeq: AS => Seq[A], cbf: CanBuildFrom[AS, A, That]) = {
+    v.slice(0, (v.length+1)/2)
+  }
+
+  def secondHalfOf[AS, A, That](v: AS)(implicit asSeq: AS => Seq[A], cbf: CanBuildFrom[AS, A, That]) = {
+    v.slice(v.length/2, v.length)
   }
   
   def diamondLines(c : Char) = {
